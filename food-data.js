222f22a1f4ed10a1ea58151a539fc5cc50dc46f6
parmarkuldeeep <81968141+parmarkuldeeep@users.noreply.github.com>
2021-08-26 12:19:13 +0530

    Add files via upload

---
+++ food-data.js
@@ -0,0 +1,29 @@
+var fooddata=[
+	{
+		name:"biryani",
+		price:"260",
+		quantity:"2"
+	},
+
+	{
+		name:"dosa",
+		price:"30",
+		quantity:"1"
+	},
+	{
+		name:"oatmeal",
+		price:"50",
+		quantity:"2"
+	},
+	{
+		name:"chicken",
+		price:"350",
+		quantity:"2"
+	},
+	{
+		name:"salad",
+		price:"20",
+		quantity:"1",
+	}
+
+];
